ba53e5b6ca3f9b4f2ebba7b4615fa4cc5dacd64e
Owen <o.oconnor7@nuigalway.ie>
2014-11-05 14:23:07 +0000

    updated application helper.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -61,7 +61,12 @@ class CartsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
