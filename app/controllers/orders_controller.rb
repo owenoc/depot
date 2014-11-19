b655752678742200b9d0ab9fad42e28482916367
Owen <o.oconnor7@nuigalway.ie>
2014-11-19 13:21:16 +0000

    added users

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,12 +33,11 @@ class OrdersController < ApplicationController
     @order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
+        format.html { redirect_to store_url, notice: 'Thank you for your order.'}
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
 
-        format.html { 
-          redirect_to store_url, notice: 'Thank you for your order.'
-        }
+
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
